b0c9214bedb54bab2b68040f265d096c13e8ed6d
ayo Dawodu <tjwizking@gmail.com>
2015-02-05 20:34:07 +0000

    make api reqs

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,36 @@
 class RegistrationsController < Devise::RegistrationsController
+	
+ # GET /products/new
+   def new
+    super
+
+  end
+
+  def create
+ 
+ 		
+	uri = URI.parse("http://text-processing.com/api/sentiment/")
+
+	http = Net::HTTP.new(uri.host, uri.port)
+
+	request = Net::HTTP::Post.new(uri.request_uri)
+	request.set_form_data({"text" => "love"})
+
+	response = http.request(request)
+   
+  
+ format.html { render :new }
+   end
+  def update
+    super
+  end
+
 	 private
  
+
+  
+
+
   def sign_up_params
     params.require(:user).permit(:BrandName, :email, :password, :password_confirmation,:logo)
   end
